f43fab1dc80c88bfaa02c9109bd06033456b2a3b
Chris Jackson <chris@cd-jackson.com>
2014-04-13 16:20:42 +0100

    Update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,19 @@ module.exports = function (grunt) {
             core: {
                 expand: false,
                 flatten: true,
-                src: ['core/blockly.js','core/block.js','core/block_svg.js','core/blocks.js','core/icon.js','core/bubble.js',
-                       'core/comment.js','core/connection.js','core/contextmenu.js','core/css.js','core/field.js','core/field_textinput.js',
-                        'core/field_angle.js','core/field_checkbox.js','core/field_colour.js','core/field_dropdown.js','core/field_image.js',
-                        'core/field_label.js','core/field_variable.js','core/flyout.js','core/generator.js','core/inject.js','core/input.js',
-                        'core/msg.js','core/mutator.js','core/names.js','core/procedures.js','core/scrollbar.js','core/tooltip.js',
-                        'core/trashcan.js','core/utils.js','core/variables.js','core/warning.js','core/widgetdiv.js','core/workspace.js',
-                        'core/xml.js'
+                src: ['core/blockly.js', 'core/block.js', 'core/block_svg.js', 'core/blocks.js', 'core/icon.js',
+                    'core/bubble.js',
+                    'core/comment.js', 'core/connection.js', 'core/contextmenu.js', 'core/css.js', 'core/field.js',
+                    'core/field_textinput.js',
+                    'core/field_angle.js', 'core/field_checkbox.js', 'core/field_colour.js', 'core/field_dropdown.js',
+                    'core/field_image.js',
+                    'core/field_label.js', 'core/field_variable.js', 'core/flyout.js', 'core/generator.js',
+                    'core/inject.js', 'core/input.js',
+                    'core/msg.js', 'core/mutator.js', 'core/names.js', 'core/procedures.js', 'core/scrollbar.js',
+                    'core/tooltip.js',
+                    'core/trashcan.js', 'core/utils.js', 'core/variables.js', 'core/warning.js', 'core/widgetdiv.js',
+                    'core/workspace.js',
+                    'core/xml.js', 'core/json.js'
                 ],
                 dest: 'build/core.js'
             },
@@ -35,24 +41,24 @@ module.exports = function (grunt) {
         },
         copy: {
 
-        msg: {
-            expand: true,
-            flatten: true,
-            src: ['msg/js/*.js'],
-            dest: 'build/msg/'
-        },
+            msg: {
+                expand: true,
+                flatten: true,
+                src: ['msg/js/*.js'],
+                dest: 'build/msg/'
+            },
             media: {
                 expand: true,
                 src: ['media/**'],
                 dest: 'build/'
             },
 
-        blockly: {
-            expand: true,
-            flatten: true,
-            src: ['extjs/Blockly.js'],
-            dest: 'build/'
-        }
+            blockly: {
+                expand: true,
+                flatten: true,
+                src: ['extjs/Blockly.js'],
+                dest: 'build/'
+            }
         }
     });
 
