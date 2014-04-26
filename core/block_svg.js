5b8af6677ff57f46918e3a8b5ce7d5591eee2f53
Chris Jackson <chris@cd-jackson.com>
2014-04-26 09:22:43 +0100

    Update changes from SVN r1712

--- core/block_svg.js
+++ core/block_svg.js
@@ -513,32 +513,30 @@ Blockly.BlockSvg.prototype.render = function () {
  * @return {number} X-coordinate of the end of the field row (plus a gap).
  * @private
  */
-Blockly.BlockSvg.prototype.renderFields_ = function (fieldList, cursorX, cursorY) {
-    if (Blockly.RTL) {
-        cursorX = -cursorX;
-    }
-    for (var t = 0, field; field = fieldList[t]; t++) {
-        // Get the dimensions of the field.
-        var fieldSize = field.getSize();
-        var fieldWidth = fieldSize.width;
-
+Blockly.BlockSvg.prototype.renderFields_ =
+    function (fieldList, cursorX, cursorY) {
         if (Blockly.RTL) {
-            cursorX -= fieldWidth;
-            field.getRootElement().setAttribute('transform',
-                    'translate(' + cursorX + ', ' + cursorY + ')');
-            if (fieldWidth) {
-                cursorX -= Blockly.BlockSvg.SEP_SPACE_X;
-            }
-        } else {
-            field.getRootElement().setAttribute('transform',
-                    'translate(' + cursorX + ', ' + cursorY + ')');
-            if (fieldWidth) {
-                cursorX += fieldWidth + Blockly.BlockSvg.SEP_SPACE_X;
+            cursorX = -cursorX;
+        }
+        for (var t = 0, field; field = fieldList[t]; t++) {
+            if (Blockly.RTL) {
+                cursorX -= field.renderSep + field.renderWidth;
+                field.getRootElement().setAttribute('transform',
+                        'translate(' + cursorX + ', ' + cursorY + ')');
+                if (field.renderWidth) {
+                    cursorX -= Blockly.BlockSvg.SEP_SPACE_X;
+                }
+            } else {
+                field.getRootElement().setAttribute('transform',
+                        'translate(' + (cursorX + field.renderSep) + ', ' + cursorY + ')');
+                if (field.renderWidth) {
+                    cursorX += field.renderSep + field.renderWidth +
+                        Blockly.BlockSvg.SEP_SPACE_X;
+                }
             }
         }
-    }
-    return Blockly.RTL ? -cursorX : cursorX;
-};
+        return Blockly.RTL ? -cursorX : cursorX;
+    };
 
 /**
  * Computes the height and widths for each row and field.
@@ -612,14 +610,19 @@ Blockly.BlockSvg.prototype.renderCompute_ = function (iconWidth) {
             // The first row gets shifted to accommodate any icons.
             input.fieldWidth += Blockly.RTL ? -iconWidth : iconWidth;
         }
+        var previousFieldEditable = false;
         for (var j = 0, field; field = input.fieldRow[j]; j++) {
             if (j != 0) {
                 input.fieldWidth += Blockly.BlockSvg.SEP_SPACE_X;
             }
             // Get the dimensions of the field.
             var fieldSize = field.getSize();
-            input.fieldWidth += fieldSize.width;
+            field.renderWidth = fieldSize.width;
+            field.renderSep = (previousFieldEditable && field.EDITABLE) ?
+                Blockly.BlockSvg.SEP_SPACE_X : 0;
+            input.fieldWidth += field.renderWidth + field.renderSep;
             row.height = Math.max(row.height, fieldSize.height);
+            previousFieldEditable = field.EDITABLE;
         }
 
         if (row.type != Blockly.BlockSvg.INLINE) {
