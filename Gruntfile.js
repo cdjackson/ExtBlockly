eeb80cd87fc14bb4187c922d0c1ce3160fdb54f8
Chris Jackson <chris@cd-jackson.com>
2014-04-26 23:15:02 +0100

    Restore XML for mutation processing

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function (grunt) {
                     'core/tooltip.js',
                     'core/trashcan.js', 'core/utils.js', 'core/variables.js', 'core/warning.js', 'core/widgetdiv.js',
                     'core/workspace.js',
-                    'core/json.js'
+                    'core/json.js', 'core/xml.js'
                 ],
                 dest: 'build/core.js'
             },
